db7f6d58e45d485bbdc981d7da401a8403c774b4
matheus.lopes <matheus.lopes@softplan.com.br>
2018-06-18 17:27:17 -0300

    Validacao sintatica com Bean Validation

--- cursomc/src/main/java/com/matheuslopes/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/matheuslopes/cursomc/services/CategoriaService.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.matheuslopes.cursomc.domain.Categoria;
+import com.matheuslopes.cursomc.dto.CategoriaDTO;
 import com.matheuslopes.cursomc.reporitories.CategoriaRepository;
 import com.matheuslopes.cursomc.services.exceptions.DataIntegrityException;
 import com.matheuslopes.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
