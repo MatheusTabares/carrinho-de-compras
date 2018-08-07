7ddd328c43e882321b39eeea7189188eb071aed2
matheus.lopes <matheus.lopes@softplan.com.br>
2018-08-07 13:53:37 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- cursomc/src/main/java/com/matheuslopes/cursomc/services/PedidoService.java
+++ cursomc/src/main/java/com/matheuslopes/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.matheuslopes.cursomc.domain.Cliente;
 import com.matheuslopes.cursomc.domain.ItemPedido;
 import com.matheuslopes.cursomc.domain.PagamentoComBoleto;
 import com.matheuslopes.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.matheuslopes.cursomc.domain.enums.EstadoPagamento;
 import com.matheuslopes.cursomc.reporitories.ItemPedidoRepository;
 import com.matheuslopes.cursomc.reporitories.PagamentoRepository;
 import com.matheuslopes.cursomc.reporitories.PedidoRepository;
+import com.matheuslopes.cursomc.security.UserSS;
+import com.matheuslopes.cursomc.services.exceptions.AuthorizationException;
 import com.matheuslopes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -71,4 +77,13 @@ public class PedidoService {
 		return obj;
 	}
 	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
