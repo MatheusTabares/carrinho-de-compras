e8b98bc51525453e00d335901c0a4580d960eb59
matheus.lopes <matheus.lopes@softplan.com.br>
2018-08-07 10:13:53 -0300

    Implementando autorizacao

--- cursomc/src/main/java/com/matheuslopes/cursomc/config/SecurityConfig.java
+++ cursomc/src/main/java/com/matheuslopes/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.matheuslopes.cursomc.security.JWTAuthenticationFilter;
+import com.matheuslopes.cursomc.security.JWTAuthorizationFilter;
 import com.matheuslopes.cursomc.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
