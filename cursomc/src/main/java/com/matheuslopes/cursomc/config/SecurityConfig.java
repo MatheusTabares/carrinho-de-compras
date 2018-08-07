295e620c63f25a733109ba83a41405b756d6de88
matheus.lopes <matheus.lopes@softplan.com.br>
2018-08-07 14:33:54 -0300

    Esqueci a senha

--- cursomc/src/main/java/com/matheuslopes/cursomc/config/SecurityConfig.java
+++ cursomc/src/main/java/com/matheuslopes/cursomc/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	private static final String[] PUBLIC_MATHERS_POST = {
-			"/clientes/**"
+			"/clientes/**",
+			"/auth/forgot/**"
 	};
 	
 	@Override
