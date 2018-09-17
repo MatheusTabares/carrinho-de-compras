b9d768a06b68dbf3c1faba1d96c9b325b0238fc3
matheus.lopes <matheus.lopes@softplan.com.br>
2018-09-17 17:34:00 -0300

    Expondo o header location nas respostas

--- cursomc/src/main/java/com/matheuslopes/cursomc/security/JWTAuthenticationFilter.java
+++ cursomc/src/main/java/com/matheuslopes/cursomc/security/JWTAuthenticationFilter.java
@@ -57,7 +57,6 @@ public class JWTAuthenticationFilter  extends UsernamePasswordAuthenticationFilt
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         res.addHeader("Authorization", "Bearer " + token);
-        res.addHeader("access-control-expose-headers", "Authorization");
         res.addHeader("access-control-expose-headers", "Authorization"); 
 	}
 	
